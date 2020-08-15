3aba8800efe468a007297a16c96be41f79fbfbe3
wangshaobo <fireshooter@163.com>
2020-08-16 01:06:00 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Wang Shaobo on 2020/8/16.
+//  Copyright © 2020 Wang Shaobo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
