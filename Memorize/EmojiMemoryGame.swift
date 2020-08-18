54459a13ce578171c38d708bca677da512abcd7f
wangshaobo <fireshooter@163.com>
2020-08-19 02:39:02 +0800

    lecture 2

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Wang Shaobo on 2020/8/19.
+//  Copyright © 2020 Wang Shaobo. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+
+    // MARK: - Access to Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
